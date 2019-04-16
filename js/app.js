8730e6ccf4af6e7f512d5400ecbd4597d7afeff7
2511921-110 <yasu1981itou@gmail.com>
2019-04-16 19:33:12 +0900

    fixed webpack.config

--- js/app.js
+++ js/app.js
@@ -1,36 +1,98 @@
-import '../scss/style.scss';
+import '../scss/bootstrap-grid.scss';
+import '../scss/fonts.scss';
+import '../scss/main.scss';
 import Vue from 'vue/dist/vue.esm.js'
 import axios from 'axios'
 import "@babel/polyfill"
+import SweetScroll from "sweet-scroll"
+import VanillaModal from 'vanilla-modal'
+import lazySizes from 'lazysizes'
 
-console.log('bundle.jsのみでCSSも読み込むよ.なんでやねん')
+// console.log('bundle.jsのみでCSSも読み込むよ.なんでやねん')
 
 window.Vue = require('vue');
+lazySizes.init();
 
-//vue file
-import twitter from './twitter.vue'
+if(document.getElementsByClassName('modal')[0]){
+const modal = new VanillaModal(
+// {
+//   open: '.my-open-class',
+//   close: '.my-close-class'
+// }
+);
+};
 
-if (document.getElementById('twitter')) {
-new Vue({
-  el: '#twitter',
-  components: {twitter},
-  template : '<twitter>'
-})
+//smooth scroll
+const sweetScroll = new SweetScroll({
+  trigger: "a[href^='#']"
+});
+
+if (document.getElementById('Nav')) {
+  // const postsInstance = new Vue({
+  //   el: "#Posts",
+  //   data() {
+  //     return {
+  //       posts: [],
+  //     }
+  //   },
+  //   mounted(){
+  //     axios(BASEURL+'posts')
+  //     .then( (res) =>{
+  //       this.posts = res.data
+  //       // console.log(res.data)
+  //     })
+  //   },
+  // })
 }
-if (document.getElementById('Posts')) {
-  const postsInstance = new Vue({
-    el: "#Posts",
-    data() {
-      return {
-        posts: [],
-      }
-    },
-    mounted(){
-      axios(BASEURL+'posts')
-      .then( (res) =>{
-        this.posts = res.data
-        // console.log(res.data)
-      })
-    },
-  })
+
+const nav_el = document.querySelector('.nav__btn');
+nav_el.addEventListener("click",()=> {
+  // console.log("クリックされたぞなもし")
+  if(document.querySelector('.navstate') === null ){
+    // console.log("固定すんぞ")
+    document.querySelector('.nav').classList.add('navstate')
+    document.querySelector('.nav').classList.remove('nav__close')
+  }else{
+    // console.log("固定解除じゃ")
+    document.querySelector('.nav').classList.add('nav__close')
+    document.querySelector('.nav').classList.remove('navstate')
+    setTimeout( function(){
+      document.querySelector('.nav').classList.remove('nav__close')
+    }, 5000 );
+  }
+}, false)
+
+
+var controls = document.querySelectorAll('.nav .menu-item-has-children');
+for (var i = 0; i < controls.length; i++) {
+  controls[i].addEventListener('click', btnClick, false);
 }
+
+function btnClick() {
+  if(this.classList.contains('subnav_state')) {
+    this.classList.remove('subnav_state');
+    this.style.height = '';
+  } else {
+    this.classList.toggle('subnav_state');
+    this.style.transition = 'none';
+    this.style.height = 'auto';
+    const contentHeight = this.getBoundingClientRect().height;
+    this.style.height = '';
+    this.style.transition = '';
+    setTimeout(() => {
+      this.style.height = `${contentHeight}px`;
+      this.classList.add('subnav_state');
+    }, 30);
+  }
+}
+
+
+const sot = document.querySelector('.sot');
+window.addEventListener('scroll', ()=> {
+  const windowHight = window.parent.screen.height;
+  if(window.pageYOffset > windowHight){
+    sot.style.opacity = '1';
+  }else if(window.pageYOffset < windowHight){
+    sot.style.opacity = '0';
+  }
+});
--- webpack.config.js
+++ webpack.config.js
@@ -12,220 +12,328 @@ const imageminJpegtran = require("imagemin-jpegtran")
 const imageminOptipng = require("imagemin-optipng")
 const imageminSvgo = require("imagemin-svgo")
 
-module.exports = {
+const MODE = "development";
+const enabledSourceMap = MODE === "development";
 
-  mode: 'development', //development or production
+module.exports = [
 
-  devtool: "source-map",
+  {
 
-  entry: './js/app.js',// エントリーポイントの設定
+    mode: MODE, //development or production
 
-  output: {// 出力する内容
-      path: path.resolve(__dirname, '../dist'),
-      // path: path.join(__dirname, 'js'),
-      filename: 'bundle.js'// 出力するファイル名
-  },
-  
-  module: {
-    // babel-loaderの設定
-    rules: [
-      {
-        test: /\.vue$/,
-        loader: 'vue-loader'
-      },
+    // devtool: "source-map",
 
-      // {
-      //   test: /\.svg$/,
-      //   loader: 'vue-svg-loader'
-      // },
+    entry: [
+      './js/app.js',
+    ],// エントリーポイントの設定
 
-      {
-        test: /\.js$/,
-        use: [
-          {
-            loader: 'babel-loader',
-            options: {
-              presets: [
-                '@babel/preset-env'
-              ]
-            }
-          }
-        ],
-        exclude: /node_modules/,
-      },
-
-      // SASS取り込み設定
-      // {
-      //   test: /\.scss/,
-      //   use: [
-      //     'vue-style-loader',
-      //     'style-loader',
-      //     {
-      //       loader: 'css-loader',
-      //       options: {
-      //         url: false
-      //       },
-      //     },
-      //     {
-      //       loader: 'sass-loader',
-      //       options: {
-      //         sourceMap: true,
-      //       }
-      //     }
-      //   ]
-      // }
+    output: {// 出力する内容
+        path: path.resolve(__dirname, '../dist'),
+        // path: path.join(__dirname, 'js'),
+        filename: 'bundle.js'// 出力するファイル名
+    },
+    
+    module: {
+      // babel-loaderの設定
+      rules: [
+        {
+          test: /\.vue$/,
+          loader: 'vue-loader'
+        },
 
         // {
-        //   test: /\.(css|sass|scss)$/,
-        //   use: [
-        //     'style-loader',
-        //     'css-loader',
-        //     'sass-loader',
-        //     {
-        //       loader: 'postcss-loader',
-        //       options: {
-        //         plugins: function () {
-        //           return [
-        //             require('autoprefixer')
-        //           ];
-        //         }
-        //       }
-        //     }
-        //   ]
+        //   test: /\.svg$/,
+        //   loader: 'vue-svg-loader'
         // },
 
         {
-          test: /\.(sa|sc|c)ss$/,
+          test: /\.js$/,
           use: [
-            MiniCssExtractPlugin.loader,
             {
-              loader: 'css-loader',
+              loader: 'babel-loader',
               options: {
-                url: false,
-                // minimize: true,
-                sourceMap: true
-              }
-            },
-            {
-              loader: 'postcss-loader',
-              options: {
-                sourceMap: true,
-                plugins: [
-                    require('cssnano')({ // cssnanoを導入
-                        preset: 'default',
-                    }),
-                    require('autoprefixer')({
-                      grid: true, // CSS Grid Layout
-                      "browsers": [
-                        "> 1%",
-                        "IE 10"
-                      ]
-                  })
+                presets: [
+                  '@babel/preset-env'
                 ]
               }
-            },
-            {
-              loader: 'sass-loader',//SASSをCSSに変換
-              options: {
-                sourceMap: true//ソースマップツールを有効
-              }
             }
-          ]
+          ],
+          exclude: /node_modules/,
         },
 
+        // SASS取り込み設定
         // {
-        //   test: /\.(sc|c|sa)ss$/,
+        //   test: /\.scss/,
         //   use: [
-        //     // vue-style-loaderをMiniCssExtractPlugin.loaderに変更
-        //     MiniCssExtractPlugin.loader,
-        //     'css-loader',
-        //     'sass-loader',
+        //     'vue-style-loader',
+        //     'style-loader',
         //     {
-        //       loader: 'postcss-loader',
+        //       loader: 'css-loader',
         //       options: {
-        //         plugins: function () {
-        //           return [
-        //             require('autoprefixer')
-        //           ];
-        //         }
+        //         url: false
+        //       },
+        //     },
+        //     {
+        //       loader: 'sass-loader',
+        //       options: {
+        //         sourceMap: true,
         //       }
         //     }
         //   ]
-        // },
+        // }
 
-        
+          {
+            test: /\.scss/, // 対象となるファイルの拡張子
+            use: [
+              // linkタグに出力する機能
+              "style-loader",
+              // CSSをバンドルするための機能
+              {
+                loader: "css-loader",
+                options: {
+                  // オプションでCSS内のurl()メソッドの取り込みを禁止する
+                  url: true,
+                  // ソースマップの利用有無
+                  // sourceMap: true,
+                  sourceMap: enabledSourceMap,
 
-        // {
-        //   test: /\.(jpe?g|png|gif|svg|ico)(\?.+)?$/,
-        //   include: [
-        //       path.resolve(__dirname, 'img')
-        //   ],
-        //   use: {
-        //       loader: 'url-loader',
-        //       options: {
-        //           // limit: 8192,
-        //           name: './assets/[name].[ext]'
-        //       }
-        //   }
-        // },
-
-        {
-          test: /\.(jpe?g|png|gif|svg)$/i,
-          use: [
-            {
-              // loader: 'url-loader?limit=8192&name=assets/[name].[ext]',
-              loader: 'url-loader',
-              // options: {
-              //   limit: 8192,
-              //   name: '../assets/[name].[ext]'
-              // }
-            },
-          ],
-          // loader: "file-loader?limit=100000&name=/wp-content/themes/origin-theme/assets/[name].[ext]"
-          // loader: "file-loader"
-        },
-
-        {
-          test: /\.(eot|otf|ttf|woff2?|svg)(\?.+)?$/,
-          include: [
-              path.resolve(__dirname, 'node_modules')
-          ],
-          use: {
-              loader: 'file-loader',
-              options: {
-                  name: './fonts/[name].[ext]'
+                  // 0 => no loaders (default);
+                  // 1 => postcss-loader;
+                  // 2 => postcss-loader, sass-loader
+                  importLoaders: 2
+                }
+              },
+              {
+                loader: "sass-loader",
+                options: {
+                  // ソースマップの利用有無
+                  sourceMap: enabledSourceMap
+                }
               }
+            ]
+          },
+
+          // {
+          //   test: /\.(sa|sc|c)ss$/,
+          //   use: [
+          //     // MiniCssExtractPlugin.loader,
+          //     {
+          //       loader: 'css-loader',
+          //       options: {
+          //         url: false,
+          //         // minimize: true,
+          //         sourceMap: true
+          //       }
+          //     },
+          //     {
+          //       loader: 'postcss-loader',
+          //       options: {
+          //         sourceMap: true,
+          //         plugins: [
+          //             require('cssnano')({ // cssnanoを導入
+          //                 preset: 'default',
+          //             }),
+          //             require('autoprefixer')({
+          //               grid: true, // CSS Grid Layout
+          //               "browsers": [
+          //                 "> 1%",
+          //                 "IE 10"
+          //               ]
+          //           })
+          //         ]
+          //       }
+          //     },
+          //     {
+          //       loader: 'sass-loader',//SASSをCSSに変換
+          //       options: {
+          //         sourceMap: true//ソースマップツールを有効
+          //       }
+          //     }
+          //   ]
+          // },
+
+
+          {
+            test: /\.(jpe?g|png|gif|svg)$/i,
+            use: [
+              {
+                // loader: 'url-loader?limit=8192&name=assets/[name].[ext]',
+                loader: 'url-loader',
+                // options: {
+                //   limit: 8192,
+                //   name: '../assets/[name].[ext]'
+                // }
+              },
+            ],
+            // loader: "file-loader?limit=100000&name=/wp-content/themes/origin-theme/assets/[name].[ext]"
+            // loader: "file-loader"
+          },
+
+          {
+            test: /\.(eot|otf|ttf|woff2?|svg)(\?.+)?$/,
+            loader: 'file-loader?name=fonts/[name].[ext]'
+            // include: [
+            //     path.resolve(__dirname, 'node_modules')
+            // ],
+            // use: {
+            //     loader: 'file-loader',
+            //     options: {
+            //         name: './fonts/[name].[ext]'
+            //     }
+            // }
           }
-        }
+      ]
+    },
+
+    plugins: [
+      new BrowserSyncPlugin({
+        host: 'localhost',
+        port: 3000,
+        server: { baseDir: ['../../bebo-lab/'] },
+        // proxy: 'http://wise.local/',
+        files: [
+          '../css/*.css',
+          '../*.php',
+          '../*.html'
+        ]
+      }),
+      new CopyWebpackPlugin([
+        {
+          from: path.resolve(__dirname, 'assets/'),
+          to: path.resolve(__dirname, '../assets/'),
+        },
+      ]),
+      new ImageminPlugin({
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        pngquant: {
+          quality: '95-100',
+        },
+      }),
+      // new MiniCssExtractPlugin({
+      //   filename: 'style.css',
+      // }),
+      new VueLoaderPlugin()
     ]
   },
 
-  plugins: [
-    new BrowserSyncPlugin({
-      host: 'localhost',
-      port: 3000,
-      // server: { baseDir: ['../../themes'] }
-      proxy: 'http://wise.local/',
-      files: [
-        '../css/*.css',
-        '../*.php'
+  {
+
+    mode: MODE, //development or production
+
+    devtool: "source-map",
+
+    entry: [
+      './scss/front.scss'
+    ],// エントリーポイントの設定
+
+    output: {// 出力する内容
+        path: path.resolve(__dirname, '../dist'),
+        filename: '[name]'
+    },
+    
+    module: {
+      // babel-loaderの設定
+      rules: [
+        {
+          test: /\.vue$/,
+          loader: 'vue-loader'
+        },
+
+        {
+          test: /\.js$/,
+          use: [
+            {
+              loader: 'babel-loader',
+              options: {
+                presets: [
+                  '@babel/preset-env'
+                ]
+              }
+            }
+          ],
+          exclude: /node_modules/,
+        },
+
+        // SASS取り込み設定
+
+          {
+            test: /\.(sa|sc|c)ss$/,
+            use: [
+              MiniCssExtractPlugin.loader,
+              {
+                loader: 'css-loader',
+                options: {
+                  url: false,
+                  // minimize: true,
+                  sourceMap: true
+                }
+              },
+              {
+                loader: 'postcss-loader',
+                options: {
+                  sourceMap: true,
+                  plugins: [
+                      require('cssnano')({ // cssnanoを導入
+                          preset: 'default',
+                      }),
+                      require('autoprefixer')({
+                        grid: true, // CSS Grid Layout
+                        "browsers": [
+                          "> 1%",
+                          "IE 10"
+                        ]
+                    })
+                  ]
+                }
+              },
+              {
+                loader: 'sass-loader',//SASSをCSSに変換
+                options: {
+                  sourceMap: true//ソースマップツールを有効
+                }
+              }
+            ]
+          },
+
+          {
+            test: /\.(jpe?g|png|gif|svg)$/i,
+            use: [
+              {
+                loader: 'url-loader',
+              },
+            ],
+          },
+
+          {
+            test: /\.(eot|otf|ttf|woff2?|svg)(\?.+)?$/,
+            include: [
+                path.resolve(__dirname, 'node_modules')
+            ],
+            use: {
+                loader: 'file-loader',
+                options: {
+                    name: './fonts/[name].[ext]'
+                }
+            }
+          }
       ]
-    }),
-    new CopyWebpackPlugin([
-      {
-        from: path.resolve(__dirname, 'assets/'),
-        to: path.resolve(__dirname, '../assets/'),
-      },
-    ]),
-    new ImageminPlugin({
-      test: /\.(jpe?g|png|gif|svg)$/i,
-      pngquant: {
-        quality: '95-100',
-      },
-    }),
-    new MiniCssExtractPlugin({
-      filename: 'style.css',
-    }),
-    new VueLoaderPlugin()
-  ]
-};
+    },
+
+    plugins: [
+    new BrowserSyncPlugin({
+        host: 'localhost',
+        port: 3000,
+        server: { baseDir: ['../../bebo-lab/'] },
+        // proxy: 'http://wise.local/',
+        files: [
+          '../css/*.css',
+          '../*.php',
+          '../*.html'
+        ]
+      }),
+      new MiniCssExtractPlugin({
+        filename: 'front.css',
+      }),
+    ]
+  }
+]
